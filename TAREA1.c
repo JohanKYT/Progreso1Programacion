1e0bfef8587d78af4be27b0e813abe6096f24286
Kevin Maquis <kevinn_calle@hotmail.com>
2023-10-06 01:23:01 -0500

    Si es un numero primo

--- TAREA1.c
+++ TAREA1.c
@@ -11,6 +11,20 @@ int main(void){
         printf ("Ingrese un número entero\n");
         scanf ("%d", &numingresado);
 
+        if (((numingresado%numingresado==0)&&(numingresado%2==0)&&(numingresado%3==0))||(numingresado==2))
+        {
+            printf("Este numero es primo");
+             
+            
+        }
+         
+         
+         else
+         {
+            printf("Este numero no es primo");
+         }
+         
+
         if (numingresado<1)
         {
             printf ("Numero ingresado no valido\n");
