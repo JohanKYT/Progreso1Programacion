f62eb9a6521b3b09e9568ffbe4943f275c055511
Kevin Maquis <kevin.maquis@udla.edu.ec>
2023-10-12 16:34:38 -0500

    Se agrego la funcion for

---
+++ TAREA2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (){
+   
+    int numeroingresado;
+    int SumaG;
+    int contador;
+
+    for (contador=1;contador<=numeroingresado;contador++)
+
+    if (numeroingresado==1)
+    {
+        
+    }
+    
+
+    return 0;
+}
