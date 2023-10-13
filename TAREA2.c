7a8d0b195acc7e8b65646cd5f9940c7e78c28cad
Kevin Maquis <kevin.maquis@udla.edu.ec>
2023-10-12 23:25:51 -0500

    Probado y corregido

--- TAREA2.c
+++ TAREA2.c
@@ -5,21 +5,26 @@ int main (){
     int numeroingresado;
     int SumaG;
     int contador;
+    printf("Ingrese el numero\n");
+    scanf ("%d",&numeroingresado);
 
-    for (contador=1;contador<=numeroingresado;contador++)
-
+    if (numeroingresado<=0)
+    {
+        printf("Este numero ingresado no es valido\n");
+    }
+    
     if (numeroingresado==1)
     {
         printf ("La suma del numero: 1 es\n");
     }
-
-    else if (numeroingresado>=2)
+    else if (numeroingresado>1)
     {
-        SumaG=(numeroingresado*(numeroingresado+1));
-    }
- 
+        for (contador=1; contador<=numeroingresado;contador++)
+        {
+         SumaG=(numeroingresado*(numeroingresado+1))/2;
+        }
         printf ("La suma del numero %d es: %d",numeroingresado,SumaG);
-    
+        }  
 
     return 0;
 }
